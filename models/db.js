ecf7252b000a6ef1eac9b14e30f18c78ba64f518
yirun0701 <yirun0701@icloud.com>
2014-02-28 16:49:58 +0800

    update

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
-var db_path = "mongodb://172.20.67.107:27017/product_price";
-//var db_path = "mongodb://localhost:27017/baby_tissue";
+// var db_path = "mongodb://172.20.67.107:27017/product_price";
+var db_path = "mongodb://localhost:27017/product_price";
 //var db_path = "localhost:27017/taobao_DA";
 exports.db_path = db_path;
 
